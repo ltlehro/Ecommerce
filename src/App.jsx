790aa96c679fa503fa51b4ebd115a16bbfd20911
zayn <ltlehro@gmail.com>
2025-06-08 22:45:03 +0500

    completed ecommerce filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import { useState } from "react";
+import Navigation from "./Navigation/Nav.jsx";
+import Products from "./Products/Products.jsx";
+import products from "./db/data.jsx";
+import Recommended from "./Recommended/Recommended.jsx";
+import Sidebar from "./Sidebar/Sidebar.jsx";
+import Card from "./components/Card.jsx";
+import "./index.css";
 
-const App = () => {
-    return (
-        <div>App</div>
-    )
+function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
+  const result = filteredData(products, selectedCategory, query);
+
+  return (
+    <>
+      <Sidebar handleChange={handleChange} />
+      <Navigation query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick} />
+      <Products result={result} />
+    </>
+  );
 }
-export default App
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
